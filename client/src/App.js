5bd1c7c1d4e76ae3184fd8e7b50ec3d9914aab18
henriquemartins <henrm@riseup.net>
2021-07-12 12:15:55 +0200

    add Components and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
-
-import React from 'react';
+import React from "react";
+import Container from "./context/Container";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
+import Register from "./Components/Register";
+import Login from "./Components/Login";
+import Footer from "./Components/Footer";
+import Posts from "./Components/Posts";
+import About from "./Components/About";
 
 function App() {
   return (
-    <div className="App">
-      <h1>My React App</h1>
-    </div>
+    <Container>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/posts" component={Posts} />
+            <Route path="/about" component={About} />
+          </Switch>
+
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </Container>
   );
 }
-
 
 export default App;
