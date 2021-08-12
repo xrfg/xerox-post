bed8af7fb1da2a1cbbc6236204a6ff75e9b7f08b
henriquemartins <henrm@riseup.net>
2021-08-12 15:22:41 +0200

    implementing Cloudinary uploads on /profile & styling

--- client/src/Pages/Profile/Profile.js
+++ client/src/Pages/Profile/Profile.js
@@ -1,46 +1,88 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Profile.scss";
 import baseURL from "../../config/baseURL";
 import axios from "axios";
 
 export default function Profile({ user }) {
   const [success, setSuccess] = useState();
+  const [image, setImage] = useState("");
+  const [url, setUrl] = useState();
+
+  useEffect(() => {
+    console.log(image);
+  }, [image]);
+
+  useEffect(() => {
+    setUrl(user.avatar);
+  }, []);
+
+  useEffect(() => {
+    console.log(url);
+  }, [url]);
+
+  const handleImgUploadAndPost = async (e) => {
+    const formData = new FormData();
+    formData.append("file", image);
+    formData.append("upload_preset", "xeroxtry");
+    formData.append("cloud_name", "xeroxcloud");
+    const options = { method: "POST", body: formData };
+    return fetch(
+      "https://api.cloudinary.com/v1_1/xeroxcloud/image/upload",
+      options
+    )
+      .then((res) => res.json())
+      .then(async (res) => {
+        setUrl(res.secure_url);
+        console.log(url);
+        const userData = {
+          username: e.target.username.value,
+          avatar: res.secure_url,
+        };
+        try {
+          const res = await axios.post(baseURL + "/users/edit", userData);
+
+          setSuccess("profile successfully updated, redirect in 1s");
+          setTimeout(() => {
+            window.location.replace("/my-posts");
+          }, 1000);
+
+          console.log("RES ==> ", res.data);
+        } catch (e) {
+          console.log(e);
+        }
+        console.log("profile ==> ", userData);
+      })
+      .catch((err) => console.log(err));
+  };
+
   const onSubmit = async (e) => {
     e.preventDefault();
-    const username = e.target.username.value;
-    const avatar = e.target.avatar.value;
-    const userData = {
-      username,
-      avatar,
-    };
-
-    try {
-      const res = await axios.post(baseURL + "/users/edit", userData);
-
-      setSuccess("profile successfully updated, redirect in 2s");
-      setTimeout(() => {
-        window.location.replace("/profile");
-      }, 2000);
-
-      console.log("RES ==> ", res.data);
-    } catch (e) {
-      console.log(e);
-    }
-    console.log("profile ==> ", userData);
+    await handleImgUploadAndPost(e);
   };
   return (
     <div className="homepage">
       <div className="formSection">
         <h1 className="registerHeader">My Profile</h1>
         <h4 className="profileName">Hi {user.username}!</h4>
+        <label className="addImg" id="overcome">
+          <input
+            type="file"
+            style={{ display: "none" }}
+            onChange={(e) => {
+              let files = e.target.files;
+              setImage(files[0]);
+              setUrl(URL.createObjectURL(files[0]));
+            }}
+          />
+        </label>
         {success && (
           <div className="" role="alert">
             {success}
           </div>
         )}
-        <img src={user.avatar} alt="Profile Avatar" className="profileAvatar" />
+        <img src={url} alt="Profile Avatar" className="profileAvatar" />
         <form className="postForm" onSubmit={onSubmit}>
-          <label>
+          <label style={{ marginTop: "4rem" }}>
             Username:
             <input
               name="username"
@@ -58,15 +100,7 @@ export default function Profile({ user }) {
               disabled
             />
           </label>
-          <label>
-            Avatar URL:
-            <input
-              name="avatar"
-              defaultValue={user.avatar}
-              type="text"
-              className="emailBox bigUrl"
-            />
-          </label>
+
           <button type="submit" className="startBtn profileBtn">
             Update
           </button>
