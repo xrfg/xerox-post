f19429afb410d8d6e065708df8a1b20dde5e363b
henriquemartins <henrm@riseup.net>
2021-08-15 12:07:29 +0200

    deployment files

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const env = require("./config/env");
-const port = env.port;
+const port = env.port || 5000;
 const cors = require("cors");
 const core = require("./middlewares/security");
 const errorsHandler = require("./middlewares/errors");
+const path = require("path");
 
 const postRoutes = require("./routes/postRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -22,8 +23,19 @@ app.use("/api/v1/posts", postRoutes);
 // http://localhost:5000/api/v1/login
 
 // app.use("/posts", auth, postRoutes);
-
 app.use(errorsHandler);
+
+// for deployment
+app.use(express.static(path.join(__dirname, "client", "build")));
+
+/* if (env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} */
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+// ends deployment
 
 app.listen(port, () =>
   console.log(`express server is running on port: ${port}`)
