586d9f41810d8f637fad0893eecdf4afa30304fc
henriquemartins <henrm@riseup.net>
2021-08-16 11:01:44 +0200

    trying to fix mongo with heroku

--- mongooseConnection.js
+++ mongooseConnection.js
@@ -3,14 +3,17 @@ const env = require("./config/env");
 const mongoURL = env.mongoURL;
 
 // create mongoose connection
-mongoose.connect(mongoURL, {
-  dbName: "blogData",
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose
+  .connect(mongoURL, {
+    dbName: "blogData",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("MongoDB has been connected"))
+  .catch((err) => console.log(err));
 
-// listening for mongoose connection
+/* // listening for mongoose connection
 mongoose.connection.on("open", () => console.log("db connected"));
 
 // listening for any error
@@ -19,7 +22,7 @@ mongoose.connection.on("error", (err) => console.log(err.message));
 // listening for disconnection
 mongoose.connection.on("disconnected", () =>
   console.log("db connection disconnected")
-);
+); */
 
 // ctrl c
 process.on("SIGINT", () => {
